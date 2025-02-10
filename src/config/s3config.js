b0c23dd91131361c686f8ae69bebd9b8c4036f52
parth <contactparth2664@gmail.com>
2025-02-10 14:17:30 +0530

    lets see

--- src/config/s3config.js
+++ src/config/s3config.js
@@ -1,13 +1,9 @@
 import { S3Client } from "bun";
 
-
-
 export const s3Client = new S3Client({
-
-  accessKeyId: process.env.S3_ACCESS_KEY_ID,
-
-  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-
-  endpoint: process.env.S3_ENDPOINT,
-
+  accessKeyId: process.env.S3_ACCESS_KEY_ID || 'minioadmin',
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || 'minioadmin',
+  endpoint: process.env.S3_ENDPOINT || 'http://minio:9000',
+  forcePathStyle: true, // Required for MinIO
+  region: 'us-east-1'   // Required but can be any value for MinIO
 });
